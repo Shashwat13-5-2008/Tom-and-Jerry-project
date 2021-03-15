742c3b9a3145c563d15fcb001b1648fe37af17ca
Shashwat13-5-2008 <76483310+Shashwat13-5-2008@users.noreply.github.com>
2021-03-15 20:03:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,42 @@
-var cat1,cat2,cat3,cat4,catGroup;
-var garden;
-var mouse1,mouse2,mouse3,mouse4,mouseGroup;
-
+var cat,mouse,garden;
 function preload() {
-    //load the images here
-  cat1 = loadImage("cat1.png");
-  cat2 = loadImage("cat2.png");
-  cat3 = loadImage("cat3.png");
-  cat4 = loadImage("cat4.png");
-  mouse1 = loadImage("mouse1.png");
-  mouse2 = loadImage("mouse2.png");
-  mouse3 = loadImage("mouse3.png");
-  mouse4 = loadImage("mouse4.png");
-  garden = loadImage("garden.png");
-
-
+    cat1 = loadAnimation("images/cat1.png");
+    cat2 = loadAnimation("images/cat2.png","images/cat3.png");
+    cat3 = loadAnimation("images/cat4.png");
+    mouse1 = loadAnimation("images/mouse1.png");
+    mouse2 = loadAnimation("images/mouse2.png","images/mouse3.png");
+    mouse3 = loadAnimation("images/mouse4.png");
+    garden1 = loadImage("images/garden.png");
 
 }
 
 function setup(){
     createCanvas(1000,800);
-    //create tom and jerry sprites here
+    cat = createSprite(870,600);
+    cat.addAnimation("tomSleeping",cat1);
+    cat.scale = 0.2;
+    mouse = createSprite(200,600);
+    mouse.addAnimation("jerryStanding",mouse1);
+    mouse.scale = 0.15;
+    
 
 }
 
 function draw() {
 
-    background(255);
+    background(garden1);
     //Write condition here to evalute if tom and jerry collide
+   if(cat.x-mouse.x<(cat.width - mouse.width)/2){
+       cat.velocityX = 0;
+       cat.addAnimation("tomLastImage",cat3);
+       cat.x = 300;
+       cat.scale = 0.2;
+       cat.changeAnimation("tomLastImage");
+       mouse.addAnimation("jerryLastImage",mouse3);
+       mouse.scale = 0.15;
+       mouse.changeAnimation("jerryLastImage");
+   }
+   console.log(cat.width,mouse.width)
 
     drawSprites();
 }
@@ -35,7 +44,15 @@ function draw() {
 
 function keyPressed(){
 
-  //For moving and changing animation write code here
+  if(keyCode === LEFT_ARROW){
+      cat.velocityX = -5;
+      cat.addAnimation("catRunning",cat2);
+      cat.changeAnimation("catRunning");
+      mouse.addAnimation("jerryTeasing",mouse2);
+      mouse.frameDelay = 25;
+      mouse.changeAnimation("jerryTeasing");
+  }
+
 
 
 }
